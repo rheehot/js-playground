e2cb0fdb2c243c214f30f67b20df488847e5a188
lerry <lerry597@gmail.com>
2021-06-01 01:39:23 +0900

    Feat calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,13 @@
+function main() {
+    // 가지고 와서 담기 
+    // div 객체가 out 변수 안에 들어감 
+    let out = document.getElementById("output");
+    out.innerHTML = "계산기 구현하기";
+
+    console.log(10 + 2);
+    console.log(10 - 2);
+    console.log(10 * 2);
+    console.log(10 / 2);
+}
+
+main();
